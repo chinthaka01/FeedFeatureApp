11b3accaa73b3fbc939133bab1fd6dd1a2049451
chinthaka01 <chinthaka01@gmail.com>
2025-12-22 20:30:22 -0500

    Initial Commit.

---
+++ FeedFeatureApp/FeedFeatureAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeedFeatureAppApp.swift
+//  FeedFeatureApp
+//
+//  Created by Chinthaka Perera on 12/22/25.
+//
+
+import SwiftUI
+
+@main
+struct FeedFeatureAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FeedFeatureAppTests/FeedFeatureAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FeedFeatureAppTests.swift
+//  FeedFeatureAppTests
+//
+//  Created by Chinthaka Perera on 12/22/25.
+//
+
+import Testing
+@testable import FeedFeatureApp
+
+struct FeedFeatureAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
