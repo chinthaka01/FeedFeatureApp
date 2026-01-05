e32d5d53446e9ed63834be62c67a66e9294421e6
chinthaka01 <chinthaka01@gmail.com>
2026-01-04 22:02:53 -0500

    Add some DTs.

--- FeedFeatureApp/FeedFeatureApp.swift
+++ FeedFeatureApp/FeedFeatureApp.swift
@@ -12,12 +12,14 @@ import FeedFeature
 @main
 struct FeedFeatureApp: App {
     let analytics = AnalyticsImpl()
-    let feedAPI = FeedFeatureAPIClient()
+    let networking = NetworkingImpl()
+    let feedAPI: FeedFeatureAPIClient
     let feedDependencies: FeedDependenciesImpl
     let feedFactory: FeedFeatureFactory
     let feature: MicroFeature
     
     init() {
+        feedAPI = FeedFeatureAPIClient(networking: networking)
         feedDependencies = FeedDependenciesImpl(feedAPI: feedAPI, analytics: analytics)
         feedFactory = FeedFeatureFactory(dependencies: feedDependencies)
         feature = feedFactory.makeFeature()
